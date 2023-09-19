3c2a2b8d61ac507058cf58b44d1eb4cf818194ca
Raed Majeed <raedam786@gmail.com>
2023-09-19 11:06:29 +0530

    added spring security and a way to login using OPENID and Formlogin, also extracting username from the authenticaton.getPrincipal()

---
+++ src/main/java/com/raedmajeed/masterspringsecurity/Controller.java
@@ -0,0 +1,35 @@
+package com.raedmajeed.masterspringsecurity;
+
+import org.ietf.jgss.Oid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+
+    @GetMapping("/")
+    @ResponseBody
+    public String homePage() {
+        return "WELCOME TO HOME PAGE";
+    }
+
+    @GetMapping("/private")
+    @ResponseBody
+    public String privatePage(Authentication authentication) {
+        return getname(authentication);
+    }
+
+    private String getname(Authentication authentication) {
+        return Optional.of(authentication.getPrincipal())
+                .filter(OidcUser.class::isInstance)
+                .map(OidcUser.class::cast)
+                .map(OidcUser::getEmail)
+                .orElseGet(authentication::getName);
+    }
+}
